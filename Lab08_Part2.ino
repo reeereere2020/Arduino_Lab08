c729547071de5dbad6ecc8e6feac2eb3487b1f4a
reeereere2020 <61824449+reeereere2020@users.noreply.github.com>
2020-06-09 20:18:26 +0800

    Add files via upload

---
+++ Lab08_Part2.ino
@@ -0,0 +1,35 @@
+#include <Ultrasonic.h>
+#define TRIGGER_PIN 13
+#define ECHO_PIN 14
+volatile boolean state = LOW;
+const byte intPin = 8;
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+
+void setup() {
+  pinMode(6,INPUT);
+  Serial.begin(9600);
+  attachInterrupt(intPin, int0, FALLING);
+}
+float cmMsec, inMsec;
+long microsec;
+
+void loop() {
+  if(digitalRead(6) == LOW) {
+    noInterrupts();
+  }
+  else {
+    interrupts();
+  } 
+  Serial.print("MS: "); Serial.print(microsec);
+  Serial.print(", CM: "); Serial.print(cmMsec);
+  Serial.print(", IN: "); Serial.println(inMsec);
+  delay(1000);
+}
+
+void int() {
+  microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
+}
+
+
